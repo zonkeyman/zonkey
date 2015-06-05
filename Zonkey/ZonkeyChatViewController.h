f570fcb469a6eed813b9bdbd4da7f2e7c7304bb7
user <user@users-MacBook.local>
2015-06-05 11:41:47 +0200

    added code - now have internal inconsistency error

--- Zonkey/ZonkeyChatViewController.h
+++ Zonkey/ZonkeyChatViewController.h
@@ -7,17 +7,29 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
 
 @interface ZonkeyChatViewController : UIViewController <UITextFieldDelegate>
 {
     UITextField             *tfEntry;
+    IBOutlet UITableView    *chatTable;
+    NSMutableArray          *chatData;
+    PF_EGORefreshTableHeaderView *_refreshHeaderView;
+    
+    NSString                *className;
+    NSString                *userName;
+    
+    BOOL _reloading;
 }
 
 @property(nonatomic, strong) IBOutlet UITextField *tfEntry;
+@property (nonatomic, retain) UITableView *chatTable;
+@property (nonatomic, retain) NSArray *chatData;
 
 -(void) registerForKeyboardNotifications;
 -(void) freeKeyboardNotifications;
 -(void) keyboardWasShown:(NSNotification*)aNotification;
 -(void) keyboardWillHide:(NSNotification*)aNotification;
+-(void)loadLocalChat;
 
 @end
